44e565308fd8de7b3250397c7a75ce976c280891
toby270205 <wu@edmeasurement.com.au>
2017-10-18 21:53:24 +1100

    Some simple recoding

---
+++ RecodeSimpleExamples.R
@@ -0,0 +1,22 @@
+#Simple recodings
+
+#Make up some item response data
+x <- c(0,1,2,NA,1,2,0,1,2,3)
+y <- c(2,1,2,3,2,1,2,2,1,2)
+z <- c(1,2,3,1,3,0,1,0,NA,1)
+A <- rbind(x,y,z)
+
+#recode zero to missing
+A[A==0] <- NA
+
+#recode missing to zero
+A[is.na(A)] <- 0
+
+#Reverse code Q8,9,10. E.g., for negatively worded questions in questionnaires
+A[,c(8,9,10)] <- 3 -A[,c(8,9,10)] 
+
+#recode from zero. E.g., the lowest category is 1, not zero. 
+B <- B - 1
+#or, just for items 2 and 10
+A[,c(2,10)] <- A[,c(2,10)] - 1
+
