2efb1564b5dad37179bf9880dacc78efc3584561
toby270205 <wu@edmeasurement.com.au>
2017-10-18 14:54:40 +1100

    Added simulated data to test code. Added line for showing deleted records

--- RemoveEmptyRecords.R
+++ RemoveEmptyRecords.R
@@ -1,5 +1,25 @@
+#Simulate some data
+#################################################################
+#  generate item reponses using the Rasch model
+#################################################################
+
+set.seed(6778)
+N <- 2000           # number of students
+theta <- rnorm( N ) # student abilities
+I <- 40             # number of items
+p1 <- plogis( outer( theta , seq( -2 , 2 , len=I ) , "-" ) )  #item difficulties from -2 to 2
+resp <- 1 * ( p1 > matrix( runif( N*I ) , nrow=N , ncol=I ) )  # item responses
+colnames(resp) <- paste("I" , 1:I, sep="")
+
+#################################################################
+#Make records 10 and 15 as all missing
+resp[c(10,15),] <- NA
+
 #Remove records with missing responses for all items
 #resp contains a matrix of item responses
 #Find records where all responses are missing.
 all.na <- apply(resp, 1, function(x){all(is.na(x))})  
 resp <- resp[!all.na,]   #Delete records with all missing responses
+
+#Record numbers of deleted records, and perhaps put this in the report appendix.
+which(all.na)
