62be405a6e189ba3dbae82d1bab4e55d6ad78dbd
toby270205 <wu@edmeasurement.com.au>
2017-10-18 17:02:00 +1100

    Deleted last blank line

--- GenerateDichotomousRaschItemResponses.R
+++ GenerateDichotomousRaschItemResponses.R
@@ -9,4 +9,3 @@ I <- 40             # number of items
 p1 <- plogis( outer( theta , seq( -2 , 2 , len=I ) , "-" ) )  #item difficulties from -2 to 2
 resp <- 1 * ( p1 > matrix( runif( N*I ) , nrow=N , ncol=I ) )  # item responses
 colnames(resp) <- paste("I" , 1:I, sep="")
-
