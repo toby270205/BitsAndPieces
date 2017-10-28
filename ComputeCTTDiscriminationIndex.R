d666a00730248d9832abfd1b5a220f652ad078de
toby270205 <wu@edmeasurement.com.au>
2017-10-29 10:27:05 +1100

    Delete ComputeCTTDiscriminationIndex.R

--- ComputeCTTDiscriminationIndex.R
+++
@@ -1,8 +0,0 @@
-#Compute CTT discrimination index.
-#Assume "resp" contains the item responses, and "score" contains each student's test score.
-#Note that students' test scores do not contain the item score for which the disc index is computed.
-score <- apply(resp,1,sum)
-disc <- apply(resp,2,
-        function(x){cor(x,score-x,use="complete.obs")})
-
-#Alternatively, use the "reliability" function in the CTT package. pBis is returned.
